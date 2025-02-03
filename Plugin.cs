b7bc71b3e1d31031d95e314051d294c178ee055a
Robert <Robert@DESKTOP-RPD0MQQ>
2025-02-03 20:58:58 +0100

    Dodaj pliki projektów.

---
+++ Plugin.cs
@@ -0,0 +1,19 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.Mono;
+
+namespace OHVTrainer;
+
+[BepInPlugin("com.dewol2.ohvtrainer", "OHV Trainer", "0.0.1")]
+[BepInProcess("OHV.exe")]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+}
